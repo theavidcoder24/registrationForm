a0b5f034ec37b238477a55f33f9f7b2cf88953c2
Mallorie Cini <malloriecini@outlook.com>
2020-05-08 12:09:12 +1000

    commit files

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// To be done when script loads
+window.onload = function() {
+    document.getElementById('formsubmit')
+    addEventListener('submit', function('event') {
+    event.preventDefault()
+    });
+}
+
+/* == Password Visibility == */
+function revealPasswords() {
+    if (password_one.getAttribute('type') === 'text') {
+        password_one.setAttribute('type', 'password');
+        password_two.setAttribute('type', 'password');
+        eye.setAttribute('class', 'fas fa-eye');
+        eye2.setAttribute('class', 'fas fa-eye');
+    } else {
+        password_one.setAttribute('type', 'text');
+        password_two.setAttribute('type', 'text');
+        eye.setAttribute('class', 'fas fa-eye-slash');
+        eye2.setAttribute('class', 'fas fa-eye-slash');
+    }
+}
+
+function comparePass() {
+    if (password_one.value === password_two.value) {
+        error.innerHTML = "Passwords Match";
+        password_one.setCustomValidity("");
+        password_two.setCustomValidity("");
+    }
+    else {
+        error.innerHTML = "Passwords don't match";
+        password_one.setCustomValidity("");
+        password_two.setCustomValidity("");
+    }
+}
+
+
+/* Agree Terms 
+function agreedChecked(literalString) {
+    console.log(literalString);
+    if (agreedChecked.checked) {
+        agreedToTerms.removeAttribute('disabled');
+    } else {
+        agreedToTerms.setAttribute('disabled', 'disabled');
+    }
+}
+*/
